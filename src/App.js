8d36998e12f49450231655107d6cac99446618f7
Merilee Wheelock <eelirem@gmail.com>
2018-07-02 12:23:23 -0700

    set up with firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+	componentWillMount() {
+		// this lifecycle method automatically runs before App is rendered
+		// firebase set up - access firebase server first!
+		firebase.initializeApp({
+			apiKey: 'AIzaSyBpv3_PBukZPYKGF7YQbyzU9TpxQEZx3IQ',
+			authDomain: 'authentication-a80a2.firebaseapp.com',
+			databaseURL: 'https://authentication-a80a2.firebaseio.com',
+			projectId: 'authentication-a80a2',
+			storageBucket: 'authentication-a80a2.appspot.com',
+			messagingSenderId: '419831118917'
+		});
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>An app</Text>
+				<Header headerText="Authentication" />
+				<LoginForm />
 			</View>
 		)
 	}
